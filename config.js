3e65a6e20cb04196409c2203449f5e1f4bcb7460
Đức Cường Phạm <cuongpd@ivstech.co.kr>
2023-08-16 09:43:14 +0700

    Update

---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {
+    texts: [
+        "Cô bé dễ thương thân mến",
+        "Chúc em thật nhiều sức khỏe,",
+        "luôn hạnh phúc,",
+        "và vui vẻ trong cuộc đời này.",
+        "Happy Birthday To You ༼ つ ◕_◕ ༽つ",
+    ],
+    imgs: {
+        "Cô bé dễ thương thân mến": "./imgs/1.jpg",
+    },
+    desc: {
+        turn_on: "BẬT ĐÈN",
+        play: "PHÁT NHẠC",
+        bannar_coming: "BẮT ĐẦU TRANG TRÍ NÀO",
+        balloons_flying: "BÓNG BAY",
+        cake_fadein: "BÁNH NGỌT",
+        light_candle: "THẮP NẾN",
+        wish_message: "",
+        story: "LỜI NHẮN DÀNH CHO EM",
+    }
+};
